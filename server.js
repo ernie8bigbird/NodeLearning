c6ad91ddf45191c7f03e87707851e691636ad988
Ernest Morgan <ernest.morgan@foodstuffs.co.nz>
2017-09-12 13:37:49 +1200

    server play

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ var fs = require('fs');
 var http = require('http');
 
 http.createServer(function(req, res){
-    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.writeHead(200, {'Content-Type': 'text/html'});
     if (req.url === '/file.txt') {
         fs.createReadStream(__dirname + '/file.txt').pipe(res);    
+    } else if (req.url === '/pluralsight.html'){
+        fs.createReadStream(__dirname + '/pluralsight.html').pipe(res);
     } else {
         res.end("Hello World");
     }
 }).listen(8080);
-console.log('port' + process.env.PORT + 'IP' + process.env.IP);
+
 console.log('Server running!');
